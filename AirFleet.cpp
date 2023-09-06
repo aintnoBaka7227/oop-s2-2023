aa7ebee9d57c16e2add3ee91f5ff7ee7d5343b06
aintnoBaka7227 <phongtrando2004@gmail.com>
2023-09-06 14:42:23 +0930

    fixed

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -7,9 +7,9 @@
 AirFleet::AirFleet() {
     fleet = new AirCraft*[5];
     fleet[0] = new Airplane(20, 10);
-    fleet[1] = new Helicopter(10000, "Blackhawk");
+    fleet[1] = new Helicopter(10000, "BlackHawk");
     fleet[2] = new AirCraft(5000);
-    fleet[3] = new Helicopter(100, "Whitehawk");
+    fleet[3] = new Helicopter(100, "WhiteHawk");
     fleet[4] = new Airplane(15,20);
 }
 
